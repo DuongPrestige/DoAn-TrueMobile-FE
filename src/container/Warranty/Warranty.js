e86c0effd66cb43a4df42e45374a08a7c7bf2201
DuongLap <duong.prestige86@gmail.com>
2024-05-28 22:40:11 +0700

    fix bao hanh

--- src/container/Warranty/Warranty.js
+++ src/container/Warranty/Warranty.js
@@ -7,7 +7,9 @@ import "./Warranty.css";
 const Warranty = () => {
   //   const [warranty, setWarranty] = useState("");
   const [seriNumber, setSeriNumber] = useState("");
+
   const [dataWarranty, setDataWarranty] = useState();
+
   const handleOnChange = async (keyword) => {
     setSeriNumber(keyword);
   };
@@ -17,12 +19,18 @@ const Warranty = () => {
     if (data && data.errCode === 0) {
       setDataWarranty(data);
     }
+
+    if (data && data.errCode === -1) {
+      setDataWarranty(data);
+    }
   };
 
   return (
     <div className="container-fluid p-4">
-      <h1 className="text-center">Kiểm tra phạm vi bảo hành của thiết bị</h1>
-      <div className="d-flex justify-content-center">
+      <h1 className="text-center mt-4">
+        Kiểm tra phạm vi bảo hành của thiết bị
+      </h1>
+      <div className="d-flex justify-content-center mt-4">
         <div className="mt-2">
           <input
             id="seriNumber"
@@ -43,59 +51,41 @@ const Warranty = () => {
           </button>
         </div>
       </div>
-      {dataWarranty ? (
+      {dataWarranty && dataWarranty.errCode === 0 ? (
         <>
-          <div className="d-flex">
+          <div className="d-flex mt-5 p-5 justify-content-center align-items-center">
             <div className="product-img">
               <img
-                style={{ width: "200px", height: "200px" }}
+                style={{ width: "300px", height: "300px" }}
                 src={dataWarranty?.image}
                 alt=""
               />
             </div>
-            <div className="mt-4">
-              <p>{dataWarranty?.nameProdudct}</p>
-              <p>{dataWarranty?.messBuyDate}</p>
-              <p>{dataWarranty?.serialNumber}</p>
+            <div className="mt-4" style={{ textAlign: "left" }}>
+              <p style={{ fontSize: "22px" }}>{dataWarranty?.nameProdudct}</p>
+              <p style={{ fontSize: "22px" }}>{dataWarranty?.messBuyDate}</p>
+              <p style={{ fontSize: "22px" }}>{dataWarranty?.serialNumber}</p>
+              <br />
+              <p style={{ fontSize: "22px" }}>
+                <u>{dataWarranty?.messWarranty}</u>
+              </p>
             </div>
           </div>
-
-          <h2>Phạm vi bảo hành</h2>
-          <br></br>
-          <div style={{ border: "1px solid #ced4da" }}>
-            <div className="p-4">
-              <h3>Bảo hành giới hạn</h3>
-              <div className="p-2">{dataWarranty?.messWarranty}</div>
-              <h5 className="p-2">
-                Gói bảo hành của bạn bao gồm các quyền lợi sau:
-              </h5>
-              <div className="d-flex">
-                <div className="single-feature">
-                  <div className="title">
-                    <h3>Bảo dưỡng phần cứng</h3>
-                  </div>
-                  <p>
-                    Sử dụng dịch vụ sửa chữa và bảo dưỡng được chứng nhận của
-                    Apple tại một trong những Nhà cung cấp dịch vụ ủy quyền của
-                    Apple
-                  </p>
-                </div>
-                <div className="ml-4 single-feature">
-                  <div className="title">
-                    <h3>Hỗ trợ qua chat & điện thoại</h3>
-                  </div>
-                  <p>
-                    Hãy liên lạc với chúng tôi qua tính năng trò chuyện hoặc
-                    điện thoại để gặp chuyên gia hỗ trợ kỹ thuật
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <br />
+          <br />
         </>
       ) : (
         <></>
       )}
+
+      {dataWarranty && dataWarranty.errCode === -1 ? (
+        <div class="d-flex justify-content-center mt-5 p-5">
+          <h2>Không tìm thấy thông tin bảo hành</h2>
+        </div>
+      ) : (
+        <></>
+      )}
+
       <div style={{ height: "300px" }}></div>
     </div>
   );
